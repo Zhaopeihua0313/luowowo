71ab8488cfdc92994088f9a954927911a352b6fa
ZhaoAlex <929214071@qq.com>
2020-06-13 19:04:24 +0800

    修改配置为开发环境配置

--- trip-mgrsite/src/main/java/cn/wolfcode/luowowo/Mgrsite.java
+++ trip-mgrsite/src/main/java/cn/wolfcode/luowowo/Mgrsite.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling           //开启排程
+//@EnableScheduling           //开启排程
 @SpringBootApplication
 public class Mgrsite {
 
